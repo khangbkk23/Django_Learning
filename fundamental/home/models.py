79f96e49acfb038caf20e059f06a5ec96203a62d
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-10-13 10:20:10 +0700

    start with database

--- fundamental/home/models.py
+++ fundamental/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Student (models.Model)
+    name = models.charField(max_length=50)
+    age = models.IntegerField(default=18)
+    hometown = models.TextField(max_length=50)
+    email = models.EmailField()
+    poitrait = models.ImageField()
+    file = models.FileField()
