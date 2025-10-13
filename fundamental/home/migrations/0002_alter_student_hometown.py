74237bb92578353ace934575109a8bcc86ecd728
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-10-13 10:52:26 +0700

    update migration

--- fundamental/home/migrations/0002_alter_student_hometown.py
+++ fundamental/home/migrations/0002_alter_student_hometown.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0001_initial'),
+        ('home', '0001_initial'), # All in 001 was copied there
     ]
 
     operations = [
