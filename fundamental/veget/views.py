916f258e384e78479332dd59c31c390b3b5c71c0
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-10-23 10:57:35 +0700

    update post method

--- fundamental/veget/views.py
+++ fundamental/veget/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def recipes(request):
     context = {'page' : 'Recipes'}
+    if request.method == "POST":
+        data = request.POST
+        
+        recipe_image = request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+        print(recipe_name,"\n")
+        print(recipe_description)
     return render(request, 'recipes.html', context=context)
