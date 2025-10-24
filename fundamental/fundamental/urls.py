9e52dca96311f1fce49a632da0c9960d8759dbac
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-10-24 16:26:20 +0700

    update image in media slash

--- fundamental/fundamental/urls.py
+++ fundamental/fundamental/urls.py
@@ -14,12 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from urllib.parse import urlparse
 from django.contrib import admin
 from django.urls import path, include
 from veget.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('recipes/', recipes, name='recipes')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+urlpatterns += staticfiles_urlpatterns()
